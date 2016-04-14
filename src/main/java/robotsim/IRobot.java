979f3ef2b2ca53354dcc0394b622511c16e54f3c
David Ensikat <densikat@gmail.com>
2016-04-14 21:05:50 +0800

    Begin writing MOVE command validation tests

---
+++ src/main/java/robotsim/IRobot.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package robotsim;
+
+/**
+ * @author David Ensikat
+ *
+ */
+public interface IRobot {
+	Boolean validateCommand(String command, int xConstraint, int yConstraint);
+	void executeCommand(String command);
+}
